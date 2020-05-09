392cca10d7208e48023fde29ea5216de103fc2fd
Disconn3ct <397465+disconn3ct@users.noreply.github.com>
2020-05-09 14:13:21 -0400

    use configurable tundev
    still need to create it automatically in startup

--- blueprints/transvpn/install.sh
+++ blueprints/transvpn/install.sh
@@ -10,7 +10,8 @@ createmount transvpn ${global_dataset_downloads}trov/complete /mnt/downloads/com
 # Check if dataset InComplete Downloads dataset exist, create if they do not.
 createmount transvpn ${global_dataset_downloads}trov/incomplete /mnt/downloads/incomplete
 
-cp ${SCRIPT_DIR}/jails/transvpn/includes/ipfw.rules /mnt/${global_dataset_iocage}/jails/transvpn/root/etc/ipfw.rules
+# Copy ipfw rules to jail, replacing #TUNDEV# with tundev (default "tun0")
+sed -e "s/#TUNDEV#/${transvpn_tundev:-tun0}/g" < ${SCRIPT_DIR}/jails/transvpn/includes/ipfw.rules > /mnt/${global_dataset_iocage}/jails/transvpn/root/etc/ipfw.rules
 
 iocage exec transvpn chown -R transmission:transmission /config
 iocage exec transvpn sysrc "transmission_enable=YES"
@@ -22,6 +23,7 @@ iocage exec transvpn mkdir /usr/local/etc/openvpn
 cp ${SCRIPT_DIR}/${transvpn_ovpnconf} /mnt/${global_dataset_iocage}/jails/transvpn/root/usr/local/etc/openvpn/
 iocage exec transvpn sysrc "firewall_enable=YES"
 iocage exec transvpn sysrc "firewall_script=/etc/ipfw.rules"
+iocage exec transvpn sed -i '' -e 's/\([[:space:]]*"rpc-whitelist-enabled":[[:space:]]*\)true,/\1false,/' /etc/ipfw.rules
 
 iocage exec transvpn service ipfw restart
 iocage exec transvpn service openvpn restart
